9a894edb77a145a81113a2992593b87426eafd77
Susana Leitão <sleitao@uporto.pt>
2020-07-24 17:21:51 +0100

    Inclusão dos pacotes de idioma (pt,de,fr)

---
+++ lang/pt/block_panopto.php
@@ -0,0 +1,65 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://127.0.0.1:8888/moodle2021
+ *
+ * @package    block
+ * @subpackage panopto
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add_to_panopto'] = 'Adicionar esta UC ao Panopto (sincronizar lista de estudantes)';
+$string['back_to_config'] = 'Voltar às configurações';
+$string['back_to_course'] = 'Voltar à UC';
+$string['block_edit_header'] = 'Selecione os conteúdos a mostrar no bloco.';
+$string['block_edit_header_help'] = 'Escolha uma pasta existente no Panopto ou crie uma nova pasta para esta UC';
+$string['completed_recordings'] = 'Gravações publicadas';
+$string['contactus'] = 'Contacte-nos';
+$string['course'] = 'UC';
+$string['course_name'] = 'Nome da Unidade Curricular';
+$string['enter'] = 'Acesso';
+$string['error_retrieving'] = 'Erro a ler UC no Panopto.';
+$string['existing_course'] = 'Selecionar conteúdos de outra UC:';
+$string['fetching_content'] = 'A carregar conteúdo Panopto...';
+$string['lastyearvideos'] = 'Pretende usar vídeos do ano anterior?';
+$string['links'] = 'Hiperligações';
+$string['live_sessions'] = 'Transmissões em direto';
+$string['no_completed_recordings'] = 'Sem gravações';
+$string['no_course_selected'] = 'Não existe qualquer UC selecionada';
+$string['no_live_sessions'] = 'Sem transmissões';
+$string['no_users_synced_desc'] = 'Com a Nova API os utilizadores inscritos a este curso terão acesso ao Panopto assim que se autenticarem no Moodle.';
+$string['or'] = 'ou';
+$string['panopto:addinstance'] = 'Adicionar um novo bloco Panopto';
+$string['panopto:myaddinstance'] = 'Adicionar um novo bloco Panopto à minha página';
+$string['pluginname'] = 'Panopto - Vídeos';
+$string['podcast_audio'] = 'Podcast Áudio';
+$string['podcast_video'] = 'Vídeo Podcast';
+$string['provision_course_link_text'] = 'Criar UC no Panopto';
+$string['provision_courses'] = 'Criar UCs no Panopto';
+$string['provision_error'] = 'Erro ao criar UC no Panopto.';
+$string['provision_successful'] = 'UC criada com sucesso no Panopto';
+$string['provisioncourseselect'] = 'Escolher UCs a criar no Panopto.';
+$string['result'] = 'Resultado';
+$string['role_map_header'] = 'Alterar mapeamento de perfis - Apenas Admins.';
+$string['show_all'] = 'Mostrar Mais';
+$string['show_less'] = 'Mostrar Menos';
+$string['take_notes'] = 'Tirar Apontamentos';
+$string['unprovisioned'] = 'Esta UC ainda não existe no Panopto.';
+$string['watch_live'] = 'Ver em direto';
