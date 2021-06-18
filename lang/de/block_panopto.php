5eca8e17f6fc2f65068d9cd7225518ec107fcac1
Susana Leitão <sleitao@uporto.pt>
2021-06-18 13:00:19 +0100

    Customizações na apresentação do bloco Panopto e introdução de idiomas
    Ficheiros de idioma (de,fr,en,pt), remove os podcast feeds, mensagem a docentes migração videos do ano anterior, link de acesso direto para o folder do curso

---
+++ lang/de/block_panopto.php
@@ -0,0 +1,65 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://moodle.up.pt
+ *
+ * @package    block
+ * @subpackage panopto
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add_to_panopto'] = 'Dieses Fach Panopto hinzufügen (Liste der Studierenden synchronisieren)';
+$string['back_to_config'] = 'Zurück zu den Einstellungen';
+$string['back_to_course'] = 'Zurück zum Fach (UC)';
+$string['block_edit_header'] = 'Wählen Sie die Inhalte aus, die in diesem Abschnitt angezeigt werden sollen.';
+$string['block_edit_header_help'] = 'Wählen Sie einen vorhandenen Ordner in Panopto oder erstellen Sie einen neuen Ordner für dieses Fach (UC).';
+$string['completed_recordings'] = 'Veröffentlichte Aufnahmen';
+$string['contactus'] = 'Kontaktiern Sie uns';
+$string['course'] = 'Fach (UC)';
+$string['course_name'] = 'Name des Fachs (UC)';
+$string['enter'] = 'Zugang';
+$string['error_retrieving'] = 'Fehler beim Lesen des Fachs im Panopto.';
+$string['existing_course'] = 'Inhalte eines anderen Fachs (UC) wählen:';
+$string['fetching_content'] = 'Laden der Panopto-Inhalte...';
+$string['lastyearvideos'] = 'Möchten Sie Videos aus dem Vorjahr nutzen?';
+$string['links'] = 'Hyperlinks';
+$string['live_sessions'] = 'Direktübertragungen';
+$string['no_completed_recordings'] = 'Ohne Aufnahmen';
+$string['no_course_selected'] = 'Es ist kein Fach (UC) ausgewählt';
+$string['no_live_sessions'] = 'Keine Direktübertragungen';
+$string['no_users_synced'] = 'Noch keine mit dem Ordner synchronisierte Benutzer';
+$string['no_users_synced_desc'] = 'Mit der neuen API haben die Benutzer, die in diesem Kurs eingeschrieben sind, Zugang zu Panopto, sobald sie sich bei Moodle angemeldet haben.';
+$string['or'] = 'or';
+$string['panopto:addinstance'] = 'Einen neuen Panopto-Abschnitt hinzufügen';
+$string['panopto:myaddinstance'] = 'Meiner Seite einen neuen Panopto-Abschnitt hinzufügen';
+$string['pluginname'] = 'Panopto - Videos';
+$string['podcast_audio'] = 'Podcast Audio';
+$string['provision_course_link_text'] = 'Erstellung eines Fachs (UC) im Panopto';
+$string['provision_courses'] = 'Erstellung von Fächern (UCs) im Panopto';
+$string['provisioncourseselect'] = 'Zu erstellende Fächer wählen.';
+$string['provision_error'] = 'Fehler beim Erstellen eines Fachs im Panopto.';
+$string['provision_successful'] = 'Fach erfolgreich bei Panopto erstellt';
+$string['result'] = 'Resultat';
+$string['show_all'] = 'Mehr zeigen';
+$string['show_less'] = 'Weniger zeigen';
+$string['students'] = 'Studierende';
+$string['take_notes'] = 'Notizen machen';
+$string['unprovisioned'] = 'Dieses Fach existiert noch nicht im Panopto.';
+$string['watch_live'] = 'Live-Ansicht';
---
+++ lang/fr/block_panopto.php
@@ -0,0 +1,64 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://moodle.up.pt
+ *
+ * @package  block
+ * @subpackage panopto
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add_to_panopto'] = 'Ajouter ce cours (UC) Panopto (synchroniser la liste des étudiants)';
+$string['back_to_config'] = 'Retour la configuration';
+$string['back_to_course'] = 'Retour au cours (UC)';
+$string['block_edit_header'] = 'Sélectionnez les contenus afficher dans le bloc.';
+$string['block_edit_header_help'] = 'Choisissez un dossier existant Panopto ou créez un nouveau dossier pour ce cours (UC).';
+$string['completed_recordings'] = 'Enregistrements publiés';
+$string['contactus'] = 'Contactez-nous';
+$string['course'] = 'Cours (UC)';
+$string['course_name'] = 'Nom du Cours (UC)';
+$string['enter'] = 'Accès';
+$string['error_retrieving'] = 'Erreur de lecture d\'information Panopto.';
+$string['existing_course'] = 'Sélectionner le contenu d\'un autre cours (UC):';
+$string['fetching_content'] = 'Chargement du contenu Panopto...';
+$string['lastyearvideos'] = 'Voulez-vous utiliser les vidéos de l\'année dernière?';
+$string['links'] = 'Liens';
+$string['live_sessions'] = 'Flux en direct';
+$string['no_completed_recordings'] = 'Pas de vidéos';
+$string['no_course_selected'] = 'Il n\'y a pas de cours (UC) sélectionné';
+$string['no_live_sessions'] = 'Pas de sessions en direct';
+$string['no_users_synced_desc'] = 'Avec la nouvelle API, tous les utilisateurs inscrits dans ce cours seront synchronisés avec Panopto lorsqu\'ils se connectent  Moodle.';
+$string['or'] = 'ou';
+$string['panopto:addinstance'] = 'Ajouter un nouveau bloc Panopto';
+$string['panopto:myaddinstance'] = 'Ajouter un nouveau bloc Panopto ma page';
+$string['pluginname'] = 'Panopto - Vidéos';
+$string['podcast_audio'] = 'Podcast Audio';
+$string['podcast_video'] = 'Vidéo Podcast';
+$string['provision_course_link_text'] = 'Ajouter le cours (UC) panopto';
+$string['provision_courses'] = 'Ajouter les cours (UCs) panopto';
+$string['provisioncourseselect'] = 'Choisissez les courses créer Panopto.';
+$string['provision_error'] = 'Erreur lors de créer le cours panopto.';
+$string['provision_successful'] = 'Cours créé avec succès Panopto';
+$string['result'] = 'Résultat';
+$string['show_all'] = 'Afficher Plus';
+$string['show_less'] = 'Afficher moins';
+$string['take_notes'] = 'Prendre des notes';
+$string['unprovisioned'] = 'Ce cours (UC) n\'existe pas encore Panopto.';
+$string['watch_live'] = 'Regarder en direct';
